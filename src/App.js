4c5edc86aa6dfc859b4c42e82ab926f9cf0ca892
Zulma Rocio <zrmartinezg@gmail.com>
2021-12-16 17:07:11 -0500

    Add display random greeting in my  Greeting component.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import  Greeting from './components/Greeting';
 import Home from './components/Home';
+import store from './redux/configureStore';
+import {Provider} from 'react-redux';
 function App() {
   return (
     <div className="App">
+      <Provider store={store}>
       <Router>
         <Routes>
           <Route path="/" element= { <Home />} />
           <Route path="/greeting" element={ <Greeting /> } />
         </Routes>
       </Router>
+      </Provider>
     </div>
   );
 }
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreeting } from '../redux/greetings/greetings';
 
 export default function Greeting() {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, []);
+  const greetingState = useSelector((state) => state.greetingsReducer.greetings);
   return (
-    <div>
-      hello world
-    </div>
-  )
+    <div className="card pt-5 mt-5">
+      <div className="card-body text-center">
+        <h1>Greeting: {greetingState.title}</h1>
+        <button className="btn btn-primary" onClick={() => window.location.reload()} > Click me! </button>     
+     </div>
+   </div>
+  );
 }
