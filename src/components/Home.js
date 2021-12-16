1d5df298f77c2f006e8c6ed9a8fdef576e56f4f5
Zulma Rocio <zrmartinezg@gmail.com>
2021-12-16 16:01:47 -0500

    Add App component with react-router, Add Greeting component that will display a greeting.

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+    <div className='text-center'>
+      <h2>HOME</h2>
+      <ul>
+        <li className='mt-5'> Open <a href='http://localhost:4000/greeting'>here</a> in your browser to see a random greeting.</li>
+      </ul>
+    </div>
+      
+    </div>
+  )
+}
---
+++ src/redux/greetings/greetings.js
@@ -0,0 +1,26 @@
+const SET_GREETINGS =  'hello-react-front-end/greetings/SET_GREETINGS';
+const initialState = {
+  greetings: [],
+}
+
+const setGreetings = (payload) => ({
+  type: SET_GREETINGS,
+  payload,
+});
+
+export const fetchGreeting = () => async (dispatch) => {
+  await fetch('http://localhost:3000/v1/greetings')
+  .then(response => response.json())
+  .then(json => dispatch(setGreetings(json)))
+}
+
+const greetingsReducer = (state = initialState, action) => {
+  switch(action.type){
+    case SET_GREETINGS:
+      return {...state, greetings: action.payload};
+    default:
+      return state;
+  }
+}
+
+export default greetingsReducer;
